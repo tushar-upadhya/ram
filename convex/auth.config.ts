e5b6d34cca8dc3d829f3e7b70774b4e15463d471
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2024-12-18 13:08:19 +0530

    .

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://joint-cat-23.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,7 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignOutButton,
-  SignUpButton,
-} from "@clerk/nextjs";
 import React from "react";
 
 const Home: React.FC = () => {
-  return (
-    <div className="">
-      <SignedOut>
-        <SignUpButton />
-      </SignedOut>
-
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-    </div>
-  );
+  return <div className="">Home</div>;
 };
 
 export default Home;
