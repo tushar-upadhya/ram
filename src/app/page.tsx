cbc726056b105b276f284796ac85cf323c959194
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2024-12-18 14:34:34 +0530

    FUC : login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home: React.FC = () => {
-  return <div className="">Home</div>;
+  return (
+    <div className="">
+      <SignInButton />
+      <SignOutButton />
+    </div>
+  );
 };
 
 export default Home;
