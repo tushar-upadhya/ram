520e6395ff7cd44d42a1249de810faa5541919be
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2024-12-18 12:53:24 +0530

    setup : clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignUpButton,
+} from "@clerk/nextjs";
 import React from "react";
 
 const Home: React.FC = () => {
-  return <div className="">Home</div>;
+  return (
+    <div className="">
+      <SignedOut>
+        <SignUpButton />
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Home;
